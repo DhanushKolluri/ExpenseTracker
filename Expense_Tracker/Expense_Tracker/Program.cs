c15bcbb53e09581cf092716e5c4abd0ede54c43c
Dhanush Kolluri <kolluridhanush123@gmail.com>
2024-01-09 13:24:45 +0530

    Created models and created tables using EF

--- Expense_Tracker/Expense_Tracker/Program.cs
+++ Expense_Tracker/Expense_Tracker/Program.cs
@@ -1,8 +1,14 @@
+using Expense_Tracker.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+//DI
+builder.Services.AddDbContext<ApplicationDbContext>(options=>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
